281eca80d73b6545ce4163a76092d4266587e287
UmbertoSerino <serumberto@gmail.com>
2024-03-06 19:22:50 +0100

    created blade index and controller Restaurant

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\FoodItem;
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index()
+    {
+        $restaurant = Restaurant::all();
+        return view('admin.restaurant.index', compact('restaurant'));
+    }
+    public function create()
+    {
+        $restaurant = new Restaurant();
+        return view('admin.restaurant.create', compact('restaurant'));
+    }
+    public function store(Request $request)
+    {
+        $restaurant = new Restaurant();
+        return redirect()->route('admin.restaurant.index', compact('restaurant'));
+    }
 }
