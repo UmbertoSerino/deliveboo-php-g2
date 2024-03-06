0fd583463ff0cc65d69cb34a0e44d89678ca2792
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-03-06 11:29:02 +0100

    Merge branch 'seeder_resurants'

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -23,7 +23,47 @@ class RestaurantSeeder extends Seeder
                 'phone_number' => 348945687,
                 'email' => 'ma_che_dici@gmail.com',
                 'image_url' => 'https://img.ilgcdn.com/sites/default/files/styles/xl/public/foto/2019/12/29/1577604943-marghe.jpg?_=1577604943',
-            ]
+            ],
+            [
+                'name' => 'Il Pizzaiuolo',
+                'vat' => 987654321,
+                'address' => 'Via Napoli, 123, Napoli (NA)',
+                'phone_number' => 348123456,
+                'email' => 'ilpizzaiuolo@gmail.com',
+                'image_url' => 'https://www.ilpizzaiuolo.it/wp-content/uploads/2020/03/il-pizzaiuolo-1.jpg',
+            ],
+            [
+                'name' => 'Ristorante da Gino',
+                'vat' => 654321987,
+                'address' => 'Piazza Garibaldi, 1, Roma (RM)',
+                'phone_number' => 333987654,
+                'email' => 'ristorantedagino@gmail.com',
+                'image_url' => 'https://media-cdn.tripadvisor.com/media/photo-s/1a/8f/71/fb/ristorante-da-gino.jpg',
+            ],
+            [
+                'name' => 'Trattoria Nonna Maria',
+                'vat' => 147258369,
+                'address' => 'Via Toscana, 5, Firenze (FI)',
+                'phone_number' => 333456789,
+                'email' => 'nonnamaria@yahoo.it',
+                'image_url' => 'https://media-cdn.tripadvisor.com/media/photo-s/0a/43/45/29/trattoria-nonna-maria.jpg',
+            ],
+            [
+                'name' => 'Osteria del Pescatore',
+                'vat' => 369852147,
+                'address' => 'Lungomare, 10, Bari (BA)',
+                'phone_number' => 333123456,
+                'email' => 'osteriadelpescatore@hotmail.com',
+                'image_url' => 'https://media-cdn.tripadvisor.com/media/photo-s/18/4e/b8/44/osteria-del-pescatore.jpg',
+            ],
+            [
+                'name' => 'Pizzeria Napoli',
+                'vat' => 258369147,
+                'address' => 'Corso Umberto, 15, Milano (MI)',
+                'phone_number' => 333789456,
+                'email' => 'pizzerianapoli@gmail.com',
+                'image_url' => 'https://www.napolibed.com/images/le-pizzerie-storiche-a-napoli-20210302-158850.jpg',
+            ],
         ];
 
 
