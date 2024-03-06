429e4aae71c27d08c3073f0143ff6f42078edcaa
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-03-06 11:29:18 +0100

    added fillables and inserted commented links with other tables

--- app/Models/FoodItem.php
+++ app/Models/FoodItem.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+/* use App\Model\Restaurant; */
+/* use App\Model\Orders; */
 
 class FoodItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'restaurant_id',
+        'description',
+        'ingredients',
+        'price',
+        'image_url',
+    ];
+
+
+    /* public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    } */
+    /* public function orders()
+    {
+        return $this->belongsToMany(Restaurant::class);
+    } */
 }
