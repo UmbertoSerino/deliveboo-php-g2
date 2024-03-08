2e91a43fc622c052f4bebf9c592684439c8c9ab4
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-03-08 21:12:13 +0100

    modified index.blade of restaurants and food items

--- resources/views/admin/fooditems/index.blade.php
+++ resources/views/admin/fooditems/index.blade.php
@@ -1,16 +1,67 @@
 @extends('layouts.admin')
 @section('main-content')
 
-<div class="container">
+<div id="food-item-index" class="container">
     <div class="row">
       <h1 class="mb-5 text-center">
-        Qui sono disponibili tutti i piatti del ristorante di: {{ Auth::user()->name }} 
+        Il tuo menu:
       </h1>
-      <div class="col-6">
-          <a href="{{ route('admin.fooditems.create') }}" class="btn btn-primary">Aggiungi un nuovo piatto</a>
-      </div>
 
-          <table class="table">
+      {{-- condizione: se non ci sono piatti --}}
+
+      @if ($foodItems->isEmpty())
+      <div class="col-12 text-center">
+          <a href="{{ route('admin.fooditems.create') }}" class="btn btn-primary">Aggiungi subito il tuo primo piatto!</a>
+      </div>
+      
+      @else
+      {{-- Se è presente almeno un piatto: --}}
+
+      <div class="col-12 mb-3">
+        <a href="{{ route('admin.fooditems.create') }}" class="btn btn-primary">Aggiungi un nuovo piatto</a>
+      </div>
+      @foreach($foodItems as $foodItem )
+      <div class="col-3">
+        <div class="food-card">
+          <div class="food-card_img">
+              <img src="{{ $foodItem->image_url }}" alt="" >
+          </div>
+          <div class="food-card_content">
+              <div class="food-card_title-section">
+                  <p class="food-card_title">{{ $foodItem->name }}</p>
+                  <p class="food-card_author">{{$foodItem->description}}</p>
+                  <p class="food-card_author">{{ $foodItem->ingredients }}</p>
+              </div>
+              <div class="food-card_bottom-section">
+                  <div class="space-between">
+                      <div class="mt-3">
+                          <span class="fa fa-fire">Disponibile:</span>
+                      </div>
+                      <div class="pull-right">
+                          <span class="badge badge-success">Veg</span>
+                      </div>
+                  </div>
+                  <hr>
+                  <div class="space-between">
+                      <div class="food-card_price">
+                          <span>{{ $foodItem->price }} €</span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
+      @endforeach
+      
+
+      @endif
+
+
+
+
+      
+
+          {{-- <table class="table">
             <thead>
               <tr>
                 <th scope="col">ID</th>
@@ -33,16 +84,14 @@
                 <td>{{ $foodItem->image_url }} </td>
                 <td>{{ $foodItem->created_at }} </td>
                 <td> 
-                  {{-- <div class="col-6">
-                      <a href="{{ route('admin.fooditems.create', $foodItems) }}" class="btn btn-primary">Aggiungi piatto</a>
-                  </div> --}}
+                 
                 </td>
                 @empty
                 <td> Non ci sono piatti al momento {{ Auth::user()->name }} </td>
                 @endforelse 
               </tr>
             </tbody>
-          </table>       
+          </table>  --}}      
         </div>
       </div>
     </div>
--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -3,54 +3,60 @@
 
 <div class="container">
     <div class="row">
-      <h1 class="mb-5 text-center">
-        Qui sono disponibili tutti i ristoranti    
-      </h1>
       @if ($restaurants->isEmpty())
-      <div class="col-6">
-          <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary">Crea un nuovo Ristorante</a>
+      <div class="col-12 text-center">
+          <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary">Inizializza Ristorante</a>
       </div>
-      @endif
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome Ristoranti</th>
-                <th scope="col">Partita Iva</th>
-                <th scope="col">Mail del ristorante</th>
-                <th scope="col">indirizzo</th>
-                <th scope='col'>numero telefonico</th>
-                <th scope='col'>immagine</th>
-                <th scope="col">Titolare</th>
-                <th scope="col">Mail del Titolare</th>
-                <th scope="col">Ristorante creato il</th>
-              </tr>
-            </thead>
-            <tbody>
-                @forelse ($restaurants as $restaurant )
-              <tr>
-                <th scope="row">{{ $restaurant->id }}</th>
-                <td>{{ $restaurant->name }}</td>
-                <td> {{$restaurant->vat}}</td>
-                <td>{{ $restaurant->email }}</td>
-                <td>{{ $restaurant->address }}</td>
-                <td>{{ $restaurant->phone_number }} </td>
-                <td>- </td>
+      @else
+      {{-- inizio card --}}
+      @foreach ($restaurants as $restaurant)
+        
+      <div class="col-12">
+      <div class="card mb-3" style="">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{ $restaurant->image_url }}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Ristorante "{{ $restaurant->name }}"" di {{ $restaurant->user->name }} {{ $restaurant->user->last_name }} </h5>
 
-                <td>{{ $restaurant->user->name }} {{ $restaurant->user->last_name }}</td>
-                <td>{{ $restaurant->user->email }}</td>
-                <td>{{ $restaurant->created_at->format('d/m/Y') }}</td>
-                <td> 
-                  <div class="col-6">
-                      <a href="{{ route('admin.fooditems.create', $restaurant) }}" class="btn btn-primary">Aggiungi piatto</a>
-                  </div>
-                </td>
-                @empty
-                <td> Non ci sono ristoranti {{ Auth::user()->name }} </td>
-                @endforelse 
-              </tr>
-            </tbody>
-          </table>       
+              <p class="card-text">Recapito ristorante: {{ $restaurant->email }}, {{ $restaurant->phone_number }}</p>
+              <p class="card-text">Recapito titolare: {{ $restaurant->user->email }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+    <div class="col-12">
+      <div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Menù</h5>
+              <p class="card-text">Aggiungi e aggiorna le tue pietanze</p>
+              
+              <form action="{{ route('admin.fooditems.index') }}" method="GET">
+                @csrf
+                <button class="btn btn-primary" type="submit">I miei piatti</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Ordini</h5>
+              <p class="card-text">Tieni sott'occhio le ordinazioni nel tuo ristorante</p>
+              <a href="#" class="btn btn-primary">I miei ordini</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      @endif
+          
         </div>
       </div>
     </div>
