8a7926bbe527c4f529d33111ba92f894a91384ba
MarcoPicca <il96marco@gmail.com>
2024-03-11 14:24:14 +0100

    added bottons fooditems

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -2,12 +2,11 @@
 @section('main-content')
 
 <div class="container">
-    <div class="row">
-      @if ($restaurants->isEmpty())
+  <div class="row">
+    @if ($restaurants->isEmpty())
       <div class="col-12 text-center">
           <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary">Inizializza Ristorante</a>
       </div>
-<<<<<<< HEAD
       @else
       {{-- inizio card --}}
       @foreach ($restaurants as $restaurant)
@@ -51,10 +50,16 @@
               <h5 class="card-title">Menù</h5>
               <p class="card-text">Aggiungi e aggiorna le tue pietanze</p>
               
-              <form action="{{ route('admin.fooditems.index') }}" method="GET">
-                @csrf
-                <button class="btn btn-primary" type="submit">I miei piatti</button>
-              </form>
+              <div class="d-flex"> 
+                  <div class="col-6">
+                      <a href="{{ route('admin.fooditems.create', $restaurant) }}" class="btn btn-primary">Aggiungi piatto</a>
+                  </div>
+                  <div class="col-6">
+                      <a href="{{ route('admin.fooditems.index', $restaurant) }}" class="btn btn-warning">I miei Piatti</a>
+                  </div>
+              </div>
+              
+              
             </div>
           </div>
         </div>
@@ -70,54 +75,7 @@
       </div>
     </div>
       @endif
-          
-=======
-      @endif
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Nome Ristorante</th>
-                <th scope="col">Partita Iva</th>
-                <th scope="col">Mail del ristorante</th>
-                <th scope="col">indirizzo</th>
-                <th scope='col'>numero telefonico</th>
-                <th scope='col'>immagine</th>
-                <th scope="col">Titolare</th>
-                <th scope="col">Mail del Titolare</th>
-                <th scope="col">Ristorante creato il</th>
-              </tr>
-            </thead>
-            <tbody>
-                @forelse ($restaurants as $restaurant )
-              <tr>
-                <td>{{ $restaurant->name }}</td>
-                <td> {{$restaurant->vat}}</td>
-                <td>{{ $restaurant->email }}</td>
-                <td>{{ $restaurant->address }}</td>
-                <td>{{ $restaurant->phone_number }} </td>
-                <td>- </td>
-
-                <td>{{ $restaurant->user->name }} {{ $restaurant->user->last_name }}</td>
-                <td>{{ $restaurant->user->email }}</td>
-                <td>{{ $restaurant->created_at->format('d/m/Y') }}</td>
-                <td class="d-flex"> 
-                  <div class="col-6">
-                      <a href="{{ route('admin.fooditems.create', $restaurant) }}" class="btn btn-primary">Aggiungi piatto</a>
-                  </div>
-                  <div class="col-6">
-                      <a href="{{ route('admin.fooditems.index', $restaurant) }}" class="btn btn-warning">Elenco Piatti</a>
-                  </div>
-                </td>
-                @empty
-                <td> Nessun Ristorante creato {{ Auth::user()->name }} </td>
-                @endforelse 
-              </tr>
-            </tbody>
-          </table>       
->>>>>>> crud-fooditem-4
-        </div>
-      </div>
-    </div>
   </div>
+</div>
 
 @endsection
