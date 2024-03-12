015bf55d6fe060f3a2161212c1eb788acec91d14
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-03-12 11:33:58 +0100

    modified redirect after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/restaurants';
+    protected $redirectTo = '/admin/restaurants/create';
 
     /**
      * Create a new controller instance.
