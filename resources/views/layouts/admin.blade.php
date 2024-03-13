b9fe2e33f75fa2501792069ffd9e6357ababd913
UmbertoSerino <serumberto@gmail.com>
2024-03-13 12:56:58 +0100

    Modified icon DeliveBoo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ Vite::asset('resources/assets/img/slack-icon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/assets/img/s-l1600.jpg') }}" type="image/x-icon">
 
 
     <!-- CSRF Token -->
