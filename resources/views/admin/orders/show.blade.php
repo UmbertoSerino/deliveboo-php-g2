bd1aac3dad67c9112dbd53e2ba016d37b55085e6
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-03-18 18:01:05 +0100

    modale aggiunta

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -33,20 +33,42 @@
                         <div class="actions mb-3 pt-3">
                             <a href="{{ route('admin.orders.edit', $order->id) }}">
                                 <button class="btn btn-primary">
-                                    Modifica  Ordine
+                                    Modifica Ordine
                                 </button>
                             </a>
                         </div>
-
-                        {{-- Delete --}}
-                        <form class="d-inline-block" action="{{ route('admin.orders.destroy', $order) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <button class="btn btn-sm btn-warning" type="submit">
-                                Delete
+                            {{-- Modal --}}
+                        <!-- Button trigger modal -->
+                        <div class="actions mb-3 pt-3">
+                            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal-{{ $order->id }}">
+                                Elimina Ordine
                             </button>
-                        </form>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal-{{ $order->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h1 class="modal-title fs-5 text-danger" id="exampleModalLabel">Deleting order...</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div> 
+                                <div class="modal-body text-black">
+                                    Vuoi eliminare l'ordine di {{ $order->customer }}?
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                                <form class="d-inline-block" action="{{ route('admin.orders.destroy', $order) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">
+                                        Delete
+                                    </button>
+                                </form>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
